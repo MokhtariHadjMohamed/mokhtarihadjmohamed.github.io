3e2c24c8b2cd247aef9f4783da47d8368354d79b
root <7adj.mo7amed@gmail.com>
2024-07-24 16:43:50 +0100

    remove flask and add indext.html

--- flaskr/routes.py
+++
@@ -1,8 +0,0 @@
-# from flask import render_template, request
-# from flaskr import app
-# from flaskr.db import get_db
-
-# @app.route('/')
-# @app.route('/index')
-# def index():
-#     return render_template('blog/index.html')
