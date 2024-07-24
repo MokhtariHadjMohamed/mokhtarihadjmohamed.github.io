97e8b00f241aa6160edb133c8d9cc30e31b9dbe8
root <7adj.mo7amed@gmail.com>
2024-07-24 16:24:18 +0100

    init flask

---
+++ flaskr/routes.py
@@ -0,0 +1,8 @@
+# from flask import render_template, request
+# from flaskr import app
+# from flaskr.db import get_db
+
+# @app.route('/')
+# @app.route('/index')
+# def index():
+#     return render_template('blog/index.html')
